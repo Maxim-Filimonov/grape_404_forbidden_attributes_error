8f7822c7d48504c4a7bae1d2eb45c8e054086a8e
Maxim Filimonov <tpaktopsp@gmail.com>
2013-05-11 13:30:12 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grape404ForbiddenAttributesError::Application.config.secret_key_base = '31c64df3f4b57ba38ca9e2347b18333dbedbfeff22cad92b6cef3cd2139e0452280ce07271401cf52a72006f6a8cc0dc71b65c4b520ddee3f57be07a7a1f5cbf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grape404ForbiddenAttributesError::Application.config.session_store :encrypted_cookie_store, key: '_grape_404_forbidden_attributes_error_session'
